6a78b9e2642847c0aa14be03d54ad561b537bd89
michaelzaref <68988036+michaelzaref@users.noreply.github.com>
2024-09-01 22:42:08 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 $host = 'ecommerce.ctsqkye6wkep.eu-north-1.rds.amazonaws.com';
-$db_name = 'ecommerce';
-$username = 'root';
-$password = 'Assem123Assem';
-$port= '3306'
-    
-$conn = new mysqli($host, $username, $password, $db_name,$port);
+$username = 'root'; // Replace with your RDS username
+$password = 'Assem123Assem'; // Password provided
+$dbname = 'ecommerce'; // Replace with your database name
+$port = 3306; // Default MySQL port
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
